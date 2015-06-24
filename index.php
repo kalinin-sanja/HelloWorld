582fea3b1c348ed73c3ab180b84f3b6d613c9ca6
Alexander-HP\Александр <kalinin-sanja@yandex.ru>
2015-06-24 17:07:13 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Александр
+ * Date: 24.06.2015
+ * Time: 9:40
+ */
+
+class main{
+
+    function main($text)
+    {
+        $a = '!!!';
+        echo $text . $a;
+    }
+}
